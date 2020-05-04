80438c7f4b231a85c3675112413246c4c0d47aea
Jacob Earle <jacob.earle@yale.edu>
2020-05-04 13:27:23 -0400

    Implemented Hangman drawing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+//initializing an array that contains the stages of the hangman image that will be printed
+
+
+fn print_man (misses: i32){
+    let image_phases : [&str ; 24] = [
+    "                     ----------------------------",
+    "                     |                          |",
+    "                     |                          |",
+    "                     |                          |",
+    "                ------------                    |",
+    "               /            \\                   |",
+    "              /   -      -   \\                  |",
+    "             |   |x|    |x|   |                 |",
+    "              \\              /                  |",
+    "               \\    ----    /                   |",
+    "                ------------                    |",
+    "                    |  |                        |",
+    "                    |  |                        |",
+    "                   /    \\                       |",
+    "                  / /||\\ \\                      |",
+    "                 / / || \\ \\                     |",
+    "                /||\\ || /||\\                    |",
+    "                     ||                         |",
+    "                    //\\\\                        |",
+    "                   //  \\\\                       |",
+    "                __//    \\\\__                    |",
+    "               |___|    |___|                   |",
+    "                                                |",
+    "                 YOU LOSE!!!        ____________|____________"];
+
+    let mut i = 0;
+    for x in image_phases.iter(){
+        if i < 4 * misses{
+            println!("{}", x);
+        }
+        else{
+            println!("");
+        }
+        i+= 1;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_man(6);
 }
