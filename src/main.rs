8e7e57ffb76407eeef1b1e780682bcba099a3db4
Jacob Earle <jacob.earle@yale.edu>
2020-05-04 14:42:23 -0400

    Added file reading and word generation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
-//initializing an array that contains the stages of the hangman image that will be printed
+extern crate rand;
+use std::io::Read;
+use rand::distributions::{Distribution, Uniform};
 
+//function to return a random integer in the range from 0 to upper_bound, excluding upper bound
+fn random_integer_in_range(upper_bound: usize) -> usize{
+    let range = Uniform::from(0..(upper_bound - 1));
+    let mut rng = rand::thread_rng();
+    let num: usize = range.sample(&mut rng);
+    num
+}
 
+//function that will read dicitionary.txt into a vector of strings and randomly select one of the strings as the secret guess
+fn get_random_word() -> String{
+    //reading file into a string buffer
+    let mut file = std::fs::File::open("dictionary.txt").unwrap();
+    let mut raw_text = String::new();
+    file.read_to_string(&mut raw_text).unwrap();
+    let split = raw_text.split("\n");
+    let string_list: Vec<&str> = split.collect();
+    
+    //generating a random number as the index of the word to be selected
+    let random_int = random_integer_in_range(string_list.len());
+
+    String::from(string_list[random_int])
+}
+
+//function that will print the hangman in 6 phases
 fn print_man (misses: i32){
     let image_phases : [&str ; 24] = [
     "                     ----------------------------",
@@ -41,5 +66,7 @@ fn print_man (misses: i32){
 }
 
 fn main() {
+    let secret:String = get_random_word();
+    println!("{}", secret);
     print_man(6);
 }
